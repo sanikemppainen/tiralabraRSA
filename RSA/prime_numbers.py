14bf39309ea0768aa143739187bee069345e2aa9
sanikemppainen <sani.kemppainen@helsinki.fi>
2023-07-29 12:53:38 +0300

    projektin yleisrakennetta

---
+++ RSA/prime_numbers.py
@@ -0,0 +1,9 @@
+"""Luo alkuluvut ja tarkistaa niiden oikeellisuuden"""
+
+#miller-rabinin seula
+def tarkistaa_alkuluvut_miller_rabin_algoritmilla(alkuluvut, n):
+    pass
+
+#erastotheen seula
+def loytaa_alkuluvut_erasthoteen_seulalla(n):
+    pass
--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,1 +1,23 @@
-# tänne koodi salaukseen
+from create_keys import loytaa_alkuluvut_erasthoteen_seulalla, tarkistaa_alkuluvut_miller_rabin_algoritmilla
+
+#TODO tesktikäyttöliittymä joka kysyy inputtia konsolista 
+    #onko kyseessä decrypt vai encrypt
+
+def main():
+    #encrypt: 
+
+    #str_viesti = input()
+    #b64_viesti = decode_viesti(str_viesti)
+        #b64 formaattiin
+    #int_viesti = b64_to_int(b64_viesti)
+
+    #ITSE SALAUS:
+    #tee alkuluvut
+    alkuluvut = loytaa_alkuluvut_erasthoteen_seulalla()
+    #500 pienimmällä tuotetulla alkuvulla alkukarsinta 
+    karsittu_lista = tarkistaa_alkuluvut_miller_rabin_algoritmilla(alkuluvut, 500)
+    #luo avaimet
+    #salaa viesti avaimilla
+    #palauta salattu viesti
+
+    #decrypt:
