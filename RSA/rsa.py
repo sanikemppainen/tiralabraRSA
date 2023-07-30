29c70cabed50f0b728a26811d2a20ac2c9811795
sanikemppainen <sani.kemppainen@helsinki.fi>
2023-07-30 21:48:54 +0300

    lisätty black ja pylint

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,23 +1,28 @@
-from create_keys import loytaa_alkuluvut_erasthoteen_seulalla, tarkistaa_alkuluvut_miller_rabin_algoritmilla
+"""kutsutaan muita metodeja"""
+# from alkuluvut import (
+#    loytaa_alkuluvut_erasthoteen_seulalla,
+#    tarkistaa_alkuluvut_miller_rabin_algoritmilla,
+# )
 
-#TODO tesktikäyttöliittymä joka kysyy inputtia konsolista 
-    #onko kyseessä decrypt vai encrypt
 
 def main():
-    #encrypt: 
+    """
+    main
+    """
+    # encrypt:
 
-    #str_viesti = input()
-    #b64_viesti = decode_viesti(str_viesti)
-        #b64 formaattiin
-    #int_viesti = b64_to_int(b64_viesti)
+    # str_viesti = input()
+    # b64_viesti = decode_viesti(str_viesti)
+    # b64 formaattiin
+    # int_viesti = b64_to_int(b64_viesti)
 
-    #ITSE SALAUS:
-    #tee alkuluvut
-    alkuluvut = loytaa_alkuluvut_erasthoteen_seulalla(500)
-    #500 pienimmällä tuotetulla alkuvulla alkukarsinta 
-    karsittu_lista = tarkistaa_alkuluvut_miller_rabin_algoritmilla(alkuluvut)
-    #luo avaimet
-    #salaa viesti avaimilla
-    #palauta salattu viesti
+    # ITSE SALAUS:
+    # tee alkuluvut
+    # alkuluvut = loytaa_alkuluvut_erasthoteen_seulalla(500)
+    # 500 pienimmällä tuotetulla alkuvulla alkukarsinta
+    # karsittu_lista = tarkistaa_alkuluvut_miller_rabin_algoritmilla(alkuluvut)
+    # luo avaimet
+    # salaa viesti avaimilla
+    # palauta salattu viesti
 
-    #decrypt:
+    # decrypt:
--- RSA/testit/test_alkuluvut.py
+++ RSA/testit/test_alkuluvut.py
@@ -1,12 +1,14 @@
-from alkuluvut import loytaa_alkuluvut_erasthoteen_seulalla, tarkistaa_alkuluvut_miller_rabin_algoritmilla
+from alkuluvut import (
+    loytaa_alkuluvut_erasthoteen_seulalla,
+    tarkistaa_alkuluvut_miller_rabin_algoritmilla,
+)
 import unittest
 
 
 class TestAlkuluvut(unittest.TestCase):
-    
     def test_ei_loyda_alkuluvuista_erasthotheen_seulalla_1(self):
         self.assertNotIn(1, loytaa_alkuluvut_erasthoteen_seulalla(10))
-    
+
     def test_ei_loyda_alkuluvuista_erasthotheen_seulalla_0(self):
         self.assertNotIn(0, loytaa_alkuluvut_erasthoteen_seulalla(10))
 
@@ -17,13 +19,19 @@ class TestAlkuluvut(unittest.TestCase):
         self.assertEqual(loytaa_alkuluvut_erasthoteen_seulalla(10), [2, 3, 5, 7])
 
     def test_tarkistaa_alkuluvut_miller_rabin_algoritmilla_ei_alkuluvuilla(self):
-        self.assertEqual(tarkistaa_alkuluvut_miller_rabin_algoritmilla([4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20], 5), [])
+        self.assertEqual(
+            tarkistaa_alkuluvut_miller_rabin_algoritmilla(
+                [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20], 5
+            ),
+            [],
+        )
 
     def test_tyhja_lista_tyhjalla_inputilla(self):
         self.assertEqual(tarkistaa_alkuluvut_miller_rabin_algoritmilla([], 10), [])
 
     def test_ei_palauta_1(self):
         self.assertEqual(tarkistaa_alkuluvut_miller_rabin_algoritmilla([1], 5), [])
+
 
 if __name__ == "__main__":
     unittest.main()
